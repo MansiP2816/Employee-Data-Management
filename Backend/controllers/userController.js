ec596189429a760beab10ac74ab2d8fe63ab80cf
MansiP2816 <mansipurohit802@gmail.com>
2025-09-27 14:31:35 +0530

    Create userController.js

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,30 @@
+const User = require('../models/userModel');
+
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Public
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// @desc    Create a new user
+// @route   POST /api/users
+// @access  Public
+const createUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const user = new User({ name, email, password });
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = { getUsers, createUser };
